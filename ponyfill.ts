525d186ebeff2e8d09ddd243dec202f38f6551c2
Aaron Huggins <aaron.huggins@gmail.com>
2022-03-14 20:21:04 -0500

    Export the whole shim api surface in ponyfills

--- ponyfill.ts
+++ ponyfill.ts
@@ -1,8 +1,32 @@
 import { configureSQLiteDB, createIndexedDB } from './lib/shim.ts'
 
 configureSQLiteDB({ memory: false })
-const indexedDB = createIndexedDB()
+const {
+  IDBCursor,
+  IDBCursorWithValue,
+  IDBDatabase,
+  IDBFactory,
+  IDBIndex,
+  IDBKeyRange,
+  IDBObjectStore,
+  IDBOpenDBRequest,
+  IDBRequest,
+  IDBTransaction,
+  IDBVersionChangeEvent,
+  indexedDB
+} = createIndexedDB()
 
 export {
+  IDBCursor,
+  IDBCursorWithValue,
+  IDBDatabase,
+  IDBFactory,
+  IDBIndex,
+  IDBKeyRange,
+  IDBObjectStore,
+  IDBOpenDBRequest,
+  IDBRequest,
+  IDBTransaction,
+  IDBVersionChangeEvent,
   indexedDB
 }
