8a423e943e98c88c63892dea70faa923eb8172ae
Aaron Huggins <aaron.huggins@gmail.com>
2022-03-14 20:17:35 -0500

    Improve types for shim exports in the global scope

--- lib/indexeddb_global.ts
+++ lib/indexeddb_global.ts
@@ -1,11 +1,44 @@
 // deno-lint-ignore-file no-empty-interface
-import { IDBFactory as IDBFactory1 } from './indexeddb.ts'
+import type {
+  IDBCursor as IDBCursor1,
+  IDBCursorWithValue as IDBCursorWithValue1,
+  IDBDatabase as IDBDatabase1,
+  IDBFactory as IDBFactory1,
+  IDBIndex as IDBIndex1,
+  IDBKeyRange as IDBKeyRange1,
+  IDBObjectStore as IDBObjectStore1,
+  IDBOpenDBRequest as IDBOpenDBRequest1,
+  IDBRequest as IDBRequest1,
+  IDBTransaction as IDBTransaction1,
+  IDBVersionChangeEvent as IDBVersionChangeEvent1
+} from './indexeddb.ts'
 
 declare global {
+  interface IDBCursor extends IDBCursor1 {}
+  interface IDBCursorWithValue extends IDBCursorWithValue1 {}
+  interface IDBDatabase extends IDBDatabase1 {}
   interface IDBFactory extends IDBFactory1 {}
+  interface IDBIndex extends IDBIndex1 {}
+  interface IDBKeyRange extends IDBKeyRange1 {}
+  interface IDBObjectStore extends IDBObjectStore1 {}
+  interface IDBOpenDBRequest extends IDBOpenDBRequest1 {}
+  interface IDBRequest extends IDBRequest1 {}
+  interface IDBTransaction extends IDBTransaction1 {}
+  interface IDBVersionChangeEvent extends IDBVersionChangeEvent1 {}
 
   interface WindowOrWorkerGlobalScope {
-    indexedDB: IDBFactory;
+    IDBCursor: IDBCursor
+    IDBCursorWithValue: IDBCursorWithValue
+    IDBDatabase: IDBDatabase
+    IDBFactory: IDBFactory
+    IDBIndex: IDBIndex
+    IDBKeyRange: IDBKeyRange
+    IDBObjectStore: IDBObjectStore
+    IDBOpenDBRequest: IDBOpenDBRequest
+    IDBRequest: IDBRequest
+    IDBTransaction: IDBTransaction
+    IDBVersionChangeEvent: IDBVersionChangeEvent
+    indexedDB: IDBFactory
   }
 
   let indexedDB: IDBFactory
