5d7e65d31b04a014fe2a8af3e36142d471d7d409
Aaron Huggins <aaron.huggins@gmail.com>
2022-03-15 19:07:19 -0500

    Update exports so symbols are correctly documented

--- ponyfill.ts
+++ ponyfill.ts
@@ -3,17 +3,17 @@ export * from "./lib/indexeddb.ts";
 
 configureSQLiteDB({ memory: false });
 
-export const {
-  IDBCursor,
-  IDBCursorWithValue,
-  IDBDatabase,
-  IDBFactory,
-  IDBIndex,
-  IDBKeyRange,
-  IDBObjectStore,
-  IDBOpenDBRequest,
-  IDBRequest,
-  IDBTransaction,
-  IDBVersionChangeEvent,
-  indexedDB,
-} = createIndexedDB();
+const api = createIndexedDB();
+
+export const IDBCursor = api.IDBCursor;
+export const IDBCursorWithValue = api.IDBCursorWithValue;
+export const IDBDatabase = api.IDBDatabase;
+export const IDBFactory = api.IDBFactory;
+export const IDBIndex = api.IDBIndex;
+export const IDBKeyRange = api.IDBKeyRange;
+export const IDBObjectStore = api.IDBObjectStore;
+export const IDBOpenDBRequest = api.IDBOpenDBRequest;
+export const IDBRequest = api.IDBRequest;
+export const IDBTransaction = api.IDBTransaction;
+export const IDBVersionChangeEvent = api.IDBVersionChangeEvent;
+export const indexedDB = api.indexedDB;
